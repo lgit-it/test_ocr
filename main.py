9a98bbf169b1f76fd9492265952ec01983e580d4
Luigi Gregori <lg@lgit.it>
2023-11-09 21:04:49 +0100

    banned words

--- main.py
+++ main.py
@@ -12,9 +12,45 @@ config.read('config.ini')
 pytesseract.pytesseract.tesseract_cmd = config['DEFAULT']['TesseractCmd']
 
 # Percorsi
+# Assumi che banned_words_file sia il percorso del tuo file di parole vietate
+banned_words_file = config['DEFAULT']['BannedWords']
+
 pdf_dir = config['DEFAULT']['PDFDirectory']
 png_dir = config['DEFAULT']['OutputDirectory']
 os.makedirs(png_dir, exist_ok=True)  # Assicurati che la directory di output esista
+
+import re
+
+def filter_text(input_text, banned_words_file):
+    """
+    Sostituisce con uno spazio tutte le parole presenti nel file esterno,
+    elimina tutte le doppie spaziature e alla fine rimuove tutte le linee
+    con lunghezza inferiore ai 4 caratteri.
+
+    :param input_text: Il testo da filtrare.
+    :param banned_words_file: Il percorso del file che contiene le parole vietate.
+    :return: Il testo filtrato.
+    """
+    # Leggi le parole vietate dal file
+    with open(banned_words_file, 'r') as file:
+        banned_words = [line.strip() for line in file.readlines()]
+
+    # Sostituisce ciascuna parola vietata con uno spazio
+    for word in banned_words:
+        input_text = re.sub(r'\b{}\b'.format(re.escape(word)), ' ', input_text)
+
+    # Elimina tutte le doppie spaziature
+    input_text = re.sub(r'\s+', ' ', input_text)
+
+    # Rimuove le righe con meno di 4 caratteri
+    filtered_lines = []
+    for line in input_text.split('\n'):
+        if len(line.strip()) >= 4:
+            filtered_lines.append(line)
+
+    return '\n'.join(filtered_lines)
+
+# ... Il resto del codice rimane invariato ...
 
 def convert_pdf_page_to_image(pdf_page):
     pix = pdf_page.get_pixmap(dpi=300)
@@ -46,11 +82,14 @@ def process_pdfs(directory, output_dir):
                 filename = f"{file_date}_{page.number}"
                 image = convert_pdf_page_to_image(page)
                 text = ocr_image_to_text(image)
+                # Filtra il testo
+                filtered_text = filter_text(text)
+
 
                 full_txt_path = os.path.join(output_dir, f"{filename}.txt")
-                save_text_to_file(text, full_txt_path)
+                save_text_to_file(filtered_text, full_txt_path)
 
-                new_pdf = create_text_pdf_page(pdf, text, page.number)
+                new_pdf = create_text_pdf_page(pdf, filtered_text, page.number)
                 new_pdf_path = os.path.join(output_dir, f"{filename}.pdf")
                 new_pdf.save(new_pdf_path)
                 new_pdf.close()
